d2b4d46eda7763491190388f1ddc2e03d52a9661
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2022-08-31 07:26:47 +0000

    Caching and using the API directly
    This is getting much closer to where I want to be. I like that the happy path reads as straight line code and that errors are handled out of band in the catch block. I think this will let me remove all of the previous API code since the tumblr api does most of that work now.
    
    I have some future work to do to extract the error handling into middleware that can be used by all of the tubmlr reuqests.
    
    I also have work to do to remove the previous caching idea. This version caches out to files which means the server can go down completely and when it comes back everything is the same as it was.
    
    Eventually I'm going to need to address the `buildRSSFeed` function to understand what's going on and make sure it's what I really want. `sigh` I'm afraid of that function. It mostly does a really good job. The feature I want to add most is the ability to proxy the images through another host.

--- src/routes/tumblr-dashboard.ts
+++ src/routes/tumblr-dashboard.ts
@@ -1,46 +1,87 @@
-import Tumblr from "tumblr.js"
+import Tumblr, { TumblrClient } from "tumblr.js"
 
-import { buildRSSItems } from '../utils/tumblr'
-import { getLoggedInUserInfo, getDashboardPosts } from '../utils/tumblr-cached-resource'
-import userStore from '../data_storage/user';
 import config from "../config/config";
-const { buildRSSFeed } = require('../utils/rss')
+import userStore from '../data_storage/user';
+import userCache from "../cache/userCache";
+import postCache from "../cache/postCache";
+
+import { buildRSSFeed } from '../utils/rss';
+import { buildRSSItems } from '../utils/tumblr'
+
+function clientForUser(user): TumblrClient {
+  return Tumblr.createClient({
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    token: user.token,
+    token_secret: user.tokenSecret,
+    returnPromises: true,
+  });
+}
+
+const ApiWithCaching = {
+  userInfo: async (client: TumblrClient): Promise<{ name: string }> => {
+    const userInfoResponse = await userCache.wrap("user/self", async () => {
+      // @ts-ignore: Promises are promised
+      return client.userInfo()
+    })
+    return userInfoResponse.user
+  },
+
+  dashboardPosts: async (client: TumblrClient): Promise<any> => {
+    const postResponse = await postCache.wrap(`posts/dashboard`, async () => {
+      // @ts-ignore: Promises are promised
+      return client.userDashboard({ limit: 60 })
+    })
+    return postResponse.posts;
+  }
+}
+
+class UserNotFoundError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "UserNotFoundError";
+  }
+}
+
+async function userFromRequest(request) {
+  const userId = request.params.userid;
+  const user: any = await userStore.findById(userId);
+
+  if (!user) {
+    throw new UserNotFoundError(`Couldn't find user ${userId}`)
+  }
+
+  return user
+}
 
 export default async function tumblrDashboard(request, response) {
   try {
-    const userId = request.params.userid;
-    const user: any = await userStore.findById(userId);
+    const user = await userFromRequest(request);
+    const cachedTumblrClient = clientForUser(user);
+    const userInfo = await ApiWithCaching.userInfo(cachedTumblrClient);
+    const posts = await ApiWithCaching.dashboardPosts(cachedTumblrClient);
 
-    if (!user) {
-      response.status(404).send("Couldn't find user");
-      return
-    }
-
-    var client = Tumblr.createClient({
-      consumer_key: config.consumer_key,
-      consumer_secret: config.consumer_secret,
-      token: user.token,
-      token_secret: user.tokenSecret
-      // TODO: returnPromises: true,
-    });
-
-    const userInfo = await getLoggedInUserInfo(client);
-    const posts = await getDashboardPosts(client);
-
-    const data = { userInfo, posts };
     const feed = buildRSSFeed({
       formatter: buildRSSItems,
       request,
-      title: `Tumblr Dashboard for ${data.userInfo.name}`,
-      description: `${data.userInfo.name} follows some interesting people. this is the stuff they post on Tumblr.`,
+      title: `Tumblr Dashboard for ${userInfo.name}`,
+      description: `${userInfo.name}'s Tumblr dashboard`,
       site_url: 'http://www.tumblr.com/dashboard',
-      data,
+      data: { userInfo, posts },
     })
 
     response.set('Content-Type', 'text/xml; charset=utf-8')
-    return response.send(feed.xml())
+    response.send(feed.xml())
+
+    return
   } catch (error) {
-    console.error(error)
+    // TODO: Extract this out to middleware - all requests are going to have this in common
+    if (error.name === "UserNotFoundError") {
+      response.status(404).send(error.message);
+      return
+    }
+
     response.status(500).send("Something went wrong");
+    return
   }
 }
