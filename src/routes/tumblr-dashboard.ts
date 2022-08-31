aeab8eca825b46a636fabe7bd4e7c3ae7206a64f
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2022-08-31 21:05:02 +0000

    API with caching and call counting

--- src/routes/tumblr-dashboard.ts
+++ src/routes/tumblr-dashboard.ts
@@ -2,8 +2,7 @@ import Tumblr, { TumblrClient } from "tumblr.js"
 
 import config from "../config/config";
 import userStore from '../data_storage/user';
-import userCache from "../cache/userCache";
-import postCache from "../cache/postCache";
+import API from "../tumblr-api/base";
 
 import { buildRSSFeed } from '../utils/rss';
 import { buildRSSItems } from '../utils/tumblr'
@@ -17,25 +16,6 @@ function clientForUser(user): TumblrClient {
     returnPromises: true,
   });
 }
-
-const ApiWithCaching = {
-  userInfo: async (client: TumblrClient): Promise<{ name: string }> => {
-    const userInfoResponse = await userCache.wrap("user/self", async () => {
-      // @ts-ignore: Promises are promised
-      return client.userInfo()
-    })
-    return userInfoResponse.user
-  },
-
-  dashboardPosts: async (client: TumblrClient): Promise<any> => {
-    const postResponse = await postCache.wrap(`posts/dashboard`, async () => {
-      // @ts-ignore: Promises are promised
-      return client.userDashboard({ limit: 60 })
-    })
-    return postResponse.posts;
-  }
-}
-
 class UserNotFoundError extends Error {
   constructor(message) {
     super(message);
@@ -54,12 +34,16 @@ async function userFromRequest(request) {
   return user
 }
 
+async function configuredClientFromRequest(request) {
+  const user = await userFromRequest(request);
+  return clientForUser(user);
+}
+
 export default async function tumblrDashboard(request, response) {
   try {
-    const user = await userFromRequest(request);
-    const cachedTumblrClient = clientForUser(user);
-    const userInfo = await ApiWithCaching.userInfo(cachedTumblrClient);
-    const posts = await ApiWithCaching.dashboardPosts(cachedTumblrClient);
+    const configuredClient = await configuredClientFromRequest(request)
+    const userInfo = await API.userInfo(configuredClient)
+    const posts = await API.dashboardPosts(configuredClient);
 
     const feed = buildRSSFeed({
       formatter: buildRSSItems,
@@ -75,6 +59,7 @@ export default async function tumblrDashboard(request, response) {
 
     return
   } catch (error) {
+    console.log(error)
     // TODO: Extract this out to middleware - all requests are going to have this in common
     if (error.name === "UserNotFoundError") {
       response.status(404).send(error.message);
---
+++ src/tumblr-api/base.ts
@@ -0,0 +1,109 @@
+import userCache from "../cache/userCache";
+import postCache from "../cache/postCache";
+
+var callCount = 0
+function CallCounter() {
+    return function (
+        target: Object,
+        key: string | symbol,
+        descriptor: PropertyDescriptor
+    ) {
+        const childFunction = descriptor.value;
+        descriptor.value = (...args: any[]) => {
+            callCount += 1
+            return childFunction.apply(this, args);
+        };
+        return descriptor;
+    };
+}
+
+function Cached({ key, cache }: { key: string, cache: any }) {
+    return function (
+        target: Object,
+        key: string | symbol,
+        descriptor: PropertyDescriptor
+    ) {
+        const childFunction = descriptor.value;
+        descriptor.value = async (...args: any[]) => {
+            return cache.wrap(key, async () => {
+                return childFunction.apply(this, args);
+            }, { ttl: 0 })
+
+        };
+        return descriptor;
+    };
+}
+
+type Blog = {
+    name: string,
+    title: string,
+    description: string,
+    url: string,
+    uuid: string,
+    updated: number
+};
+
+type UserInfoResponse = {
+    name: string,
+    likes: number,
+    following: number,
+    default_post_format: string,
+    blogs: Array<Blog>
+}
+
+type Tag = any;
+
+type Post = {
+    type: string,
+    blog_name: string,
+    blog: Blog,
+    id: string,
+    id_string: string,
+    post_url: string,
+    slug: string,
+    date: string,
+    timestamp: number,
+    state: string,
+    format: string,
+    reblog_key: string,
+    tags: Array<Tag>,
+    short_url: string,
+    summary: string,
+    should_open_in_legacy: boolean,
+    recommended_source: string | null,
+    recommended_color: string | null,
+    followed: boolean,
+    liked: boolean,
+    note_count: number,
+    title: string,
+    body: string,
+    reblog: any,
+    trail: Array<any>,
+    can_like: boolean,
+    interactability_reblog: string,
+    can_reblog: boolean,
+    can_send_in_message: boolean,
+    can_reply: boolean,
+    display_avatar: boolean
+}
+
+
+export class TumblrAPIClient {
+    @Cached({ key: "user/self", cache: userCache })
+    @CallCounter()
+    async userInfo(client): Promise<UserInfoResponse> {
+        const response = await client.userInfo();
+        return response.user
+    }
+
+    @Cached({ key: "posts/dashboard", cache: postCache })
+    @CallCounter()
+    async dashboardPosts(client): Promise<Array<Post>> {
+        const response = await client.userDashboard({ limit: 60 })
+        console.log(response.posts[0])
+        return response.posts
+    }
+}
+
+const defaultClient = new TumblrAPIClient();
+export default defaultClient;
